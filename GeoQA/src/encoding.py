c54e15ce8380e2a63f8720e37d5daa60de1a52d4
PhongNTDo <thuanphong180100@gmail.com>
2025-05-23 18:56:16 +0700

    add geoqa

---
+++ GeoQA/src/encoding.py
@@ -0,0 +1,12 @@
+import torch
+from FlagEmbedding import FlagModel
+from tqdm import tqdm
+import numpy as np
+from src.utils import logging
+
+class Encoder:
+    def __init__(self, checkpoint, query_maxlen, doc_maxlen, device='cuda'):
+        
+
+    def _encode(self, texts, is_query):
+        
